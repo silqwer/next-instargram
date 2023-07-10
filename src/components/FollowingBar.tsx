147ce0c034907f713b9999217441a170a2c4bb06
silqwer <silqwer@gmail.com>
2023-07-11 08:19:18 +0900

    feat: /api/me (#18)
    - 로그인한 유저의 팔로워, 팔로잉, 북마크 정보 가져오기

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -3,12 +3,8 @@
 import useSWR from "swr";
 
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR("/api/hello");
+  const { data, isLoading, error } = useSWR("/api/me");
   console.log(data);
-
-  // 1. 클라이언트 컴포넌트에서 백엔드에게 api/me를 통해서 사용자의 정보를 받아옴
-
-  // 2. 백엔드에서는 현재 로그인된 사용자의 세션 정보를 이용해서
 
   // 3. 백엔드에서 사용자의 상세 정보를 sanity에서 가지고 옮 (followings)
 
