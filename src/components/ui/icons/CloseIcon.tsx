aa30f4291325730752cbee52e24fdb232bb802a9
silqwer <silqwer@gmail.com>
2023-07-18 08:18:34 +0900

    Feat detail dialog (#29)
    * feat: create port component
    
    * feat: detail dialog

---
+++ src/components/ui/icons/CloseIcon.tsx
@@ -0,0 +1,4 @@
+import { CgClose } from "react-icons/cg";
+export default function CloseIcon() {
+  return <CgClose className="w-5 h-5" />;
+}
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,4 +1,4 @@
-import { SimplePost } from "@/model/post";
+import { FullPost, SimplePost } from "@/model/post";
 import { client, urlFor } from "./sanity";
 
 const simplePostProjection = `
@@ -24,4 +24,21 @@ export async function getFollowingPostsOf(username: string) {
     .then((posts) =>
       posts.map((post: SimplePost) => ({ ...post, image: urlFor(post.image) }))
     );
+}
+
+export async function getPost(id: string) {
+  return client
+    .fetch(
+      `*[_type == "post" && _id == "${id}"][0]{
+        ...,
+        "username": author->username,
+        "userImage": author->image,
+        "image": photo,
+        "likes": likes[]->username,
+        comments[]{comment, "username": author->username, "image": author->image},
+        "id":_id,
+        "createdAt": _createdAt
+      }`
+    )
+    .then((post) => ({ ...post, image: urlFor(post.image) }));
 }
