ff7a8da4ed76066af93d4352c91aba5c02c9d43e
silqwer <silqwer@gmail.com>
2023-07-22 19:43:23 +0900

    feat: toggle button

--- src/components/ui/posts/ActionBar.tsx
+++ src/components/ui/posts/ActionBar.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import HeartIcon from "../icons/HeartIcon";
 import BookmarkIcon from "../icons/BookmarkIcon";
 import { parseDate } from "@/util/date";
+import { useState } from "react";
+import ToggleButton from "../buttons/ToggleButton";
+import HeartFillIcon from "../icons/HeartFillIcon";
+import BookmarkFillIcon from "../icons/BookmarkFillIcon";
 
 type Props = {
   likes: string[];
@@ -9,11 +15,24 @@ type Props = {
   createdAt: string;
 };
 export default function ActionBar({ likes, username, text, createdAt }: Props) {
+  const [liked, setLiked] = useState(false);
+  const [bookmarked, setBookmarked] = useState(false);
+
   return (
     <>
       <div className="flex justify-between px-4 my-2">
-        <HeartIcon />
-        <BookmarkIcon />
+        <ToggleButton
+          toggled={liked}
+          onToggle={setLiked}
+          onIcon={<HeartFillIcon />}
+          offIcon={<HeartIcon />}
+        />
+        <ToggleButton
+          toggled={bookmarked}
+          onToggle={setBookmarked}
+          onIcon={<BookmarkFillIcon />}
+          offIcon={<BookmarkIcon />}
+        />
       </div>
       <div className="px-4 py-1">
         <p className="mb-2 text-sm font-bold">
