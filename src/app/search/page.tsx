8678ad532bc0943bc0a70d9a7ed10f63c13ec160
silqwer <silqwer@gmail.com>
2023-07-22 11:10:49 +0900

    feat: seo

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,9 +1,17 @@
 import UserSearch from "@/components/UserSearch";
+import { Metadata } from "next";
+
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "User Search",
+  description: "Search users to follow",
+};
 
 export default function SearchPage() {
   return (
     <div>
-      <UserSearch></UserSearch>
+      <UserSearch />
     </div>
   );
 }
