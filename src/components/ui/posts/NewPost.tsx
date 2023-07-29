36ba2d5f777006c97a692e1a932cf156e7f657a3
silqwer <silqwer@gmail.com>
2023-07-29 09:38:52 +0900

    style: new post

--- src/components/ui/posts/NewPost.tsx
+++ src/components/ui/posts/NewPost.tsx
@@ -4,7 +4,10 @@ import { AuthUser } from "@/model/user";
 import PostUserAvatar from "./PostUserAvatar";
 import FilesIcon from "../icons/FilesIcon";
 import Button from "../buttons/Button";
-import { ChangeEvent, useState, DragEvent } from "react";
+import { ChangeEvent, useState, DragEvent, FormEvent, useRef } from "react";
+import Image from "next/image";
+import GridSpinner from "../spinner/GridSpinner";
+import { useRouter } from "next/navigation";
 
 type Props = {
   user: AuthUser;
@@ -13,14 +16,16 @@ export default function NewPost({ user }: Props) {
   const { username, image } = user;
   const [dragging, setDragging] = useState(false);
   const [file, setFile] = useState<File>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
-    console.log(123123);
     const files = event.target?.files;
     if (files && files[0]) {
       setFile(files[0]);
-      console.log(files[0]);
     }
   };
   const handleDrag = (event: DragEvent) => {
@@ -37,7 +42,6 @@ export default function NewPost({ user }: Props) {
 
   const handleDrop = (event: DragEvent) => {
     event.preventDefault();
-    console.log(23);
     setDragging(true);
     const files = event.dataTransfer?.files;
     if (files && files[0]) {
@@ -46,10 +50,44 @@ export default function NewPost({ user }: Props) {
     }
   };
 
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (!file) {
+      return;
+    }
+
+    setLoading(true);
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("text", textRef.current?.value ?? "");
+
+    fetch("/api/posts/", { method: "POST", body: formData })
+      .then((response) => {
+        if (!response.ok) {
+          setError(`${response.status} ${response.statusText}`);
+          return;
+        }
+
+        router.push("/");
+      })
+      .catch((error) => setError(error.toString()))
+      .finally(() => setLoading(false));
+  };
+
   return (
-    <section>
+    <section className="flex flex-col items-center w-full max-w-xl mt-6">
+      {loading && (
+        <div className="absolute inset-0 z-20 text-center pt-[50%] bg-sky-500/20">
+          <GridSpinner />
+        </div>
+      )}
+      {error && (
+        <p className="w-full p-4 mb-4 font-bold text-center text-red-600 bg-red-100">
+          {error}
+        </p>
+      )}
       <PostUserAvatar name={username} image={image ?? ""} />
-      <form>
+      <form className="flex flex-col w-full mt-2" onSubmit={handleSubmit}>
         <input
           className="hidden"
           name="input"
@@ -59,16 +97,39 @@ export default function NewPost({ user }: Props) {
           onChange={handleChange}
         />
         <label
+          className={`w-full h-60 flex flex-col items-center justify-center ${
+            !file && "border-2 border-sky-500 border-dashed"
+          }`}
           htmlFor="input-upload"
           onDragEnter={handleDrag}
           onDragLeave={handleDrag}
           onDragOver={handleDragOver}
-          onDrag={handleDrop}
+          onDrop={handleDrop}
         >
-          <FilesIcon />
-          <p>Drag and Drop your image here or click</p>
+          {dragging && (
+            <div className="absolute inset-0 z-10 pointer-events-none bg-sky-500/20" />
+          )}
+          {!file && (
+            <div className="flex flex-col items-center pointer-events-none">
+              <FilesIcon />
+              <p>Drag and Drop your image here or click</p>
+            </div>
+          )}
+          {file && (
+            <div className="relative w-full aspect-square">
+              <Image
+                className="object-cover"
+                src={URL.createObjectURL(file)}
+                alt="local file"
+                fill
+                sizes="650px"
+              />
+            </div>
+          )}
         </label>
         <textarea
+          ref={textRef}
+          className="text-lg border outline-none border-neutral-300"
           name="text"
           id="input-text"
           required
